62e2f2f1c5420cc8ac8d567a1ecad2713ada9d28
mdtalhaikram <mdtalhaikram2502@gmail.com>
2024-12-20 21:56:51 +0500

    Calculater ,Html/Java Sacript/CSS
    Create a Calculater Web

--- script.js
+++ script.js
@@ -1,54 +1,26 @@
-console.log("welcom to Spotify")
+let calculateResult = ()=>{
+    let studentName = document.getElementById("stud-name").value;
+    let totalMarks = document.getElementById("total-marks").value;
+    let marksObtained = document.getElementById("obt-marks").value;
+    let result = (marksObtained / totalMarks) * 100;
 
-// Initialize the Variables
-let songindex = 0;  
-let AudioudioElement = new Audio ('1.mp3');
-let masterPlay = document.getElementById ('masterPlay');
-let myProgressBar = document.getElementById ('myProgressBar');
-let song = [
-    {songName:"O Maho", filePath:"song/1.mp3", coverPath:"cover/1jpg"},
-    {songName:"Dil Mujhey de agar", filePath:"song/1.mp3", coverPath:"cover/1jpg"},
-    {songName:"kahne Ko Pass Apny", filePath:"song/1.mp3", coverPath:"cover/1jpg"},
-    {songName:"Dil Ka Darya Bahhi Gaya", filePath:"song/1.mp3", coverPath:"cover/1jpg"},
-    {songName:"Sunraha He Na Keun Rorahi Hon Main", filePath:"song/1.mp3", coverPath:"cover/1jpg"},
-    {songName:"O Maho", filePath:"song/1.mp3", coverPath:"cover/1jpg"},
-    {songName:"O Maho", filePath:"song/1.mp3", coverPath:"cover/1jpg"},
-    {songName:"O Maho", filePath:"song/1.mp3", coverPath:"cover/1jpg"},
-    {songName:"O Maho", filePath:"song/1.mp3", coverPath:"cover/1jpg"},
-    {songName:"O Maho", filePath:"song/1.mp3", coverPath:"cover/1jpg"},
-    {songName:"O Maho", filePath:"song/1.mp3", coverPath:"cover/1jpg"},
-    {songName:"O Maho", filePath:"song/1.mp3", coverPath:"cover/1jpg"},
-    {songName:"O Maho", filePath:"song/1.mp3", coverPath:"cover/1jpg"},
-    {songName:"O Maho", filePath:"song/1.mp3", coverPath:"cover/1jpg"},
-]
-let audioElement = new Audio('1.mp3');
-// AudioElement.play(); 
-// Handle Play/pause click
-    masterPlay.addEventListener('click', ()=>{
-    if(audioElement.paused || audioElement.currentTime<=0){
-        audioElement.play();
-        masterPlay.classList.remove('fa-play-circle');
-        masterPlay.classList.add('fa-pause-circle');
+    if(isNaN(result) || result > 100){
+        document.getElementById("message").innerHTML="Invalid/Incomplete inputs provided.<br/>Please refresh browser and provide valid inputs"; 
+    }
+    else if(result>=90 && result<=100){
+        document.getElementById("message").innerHTML=`${studentName} has secured grade A with ${result}% marks<br/>Please refresh browser to start again `; 
+    }
+    else if(result >=80 && result < 90){
+        document.getElementById("message").innerHTML=`${studentName} has secured grade B with ${result}% marks<br/>Please refresh browser to start again `; 
+    }
+    else if (result >=70 && result < 80){
+        document.getElementById("message").innerHTML=`${studentName} has secured grade C with ${result}% marks<br/>Please refresh browser to start again `; 
+    }
+    else if(result >=60 && result < 70){
+        document.getElementById("message").innerHTML=`${studentName} has secured grade D with ${result}% marks<br/>Please refresh browser to start again `; 
     }
     else{
-        audioElement.pause();
-        masterPlay.classList.remove('fa-pause-circle');
-        masterPlay.classList.add('fa-play-circle');
+        document.getElementById("message").innerHTML=`${studentName} has secured grade F with ${result}% marks<br/>Please refresh browser to start again `; 
     }
-})
-// Listen to Events
-myProgressBar.addEventListener('timeupdate', ()=>{
-   console.log ('timeupdate')
-   // Update Seekbar 
-   console.log(songindex.Audio);
-   define([
-    'require',
-    'dependency' 
-   ], function(require, factory) { 
-    'use strict';
-    SVGAnimatedPreserveAspectRatio
-    console.log(onemptied);
-   }); 
-
-
-})
+    
+}
